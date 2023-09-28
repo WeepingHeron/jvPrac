9f5710daa187f124994fc333f3f7dace86042745
WeepingHeron <kimtaehoon_98@naver.com>
2023-09-28 17:22:31 +0900

    BlackBox

--- src/chap_07/BlackBox.java
+++ src/chap_07/BlackBox.java
@@ -6,4 +6,26 @@ public class BlackBox {
     int price; //가격
     String color; //색상
     static boolean canAutoReport = false; // 자동 신고 기능
+
+    void autoReport() {
+        if (canAutoReport) {
+            System.out.println("충돌이 감지되어 자동으로 신고합니다.");
+        } else {
+            System.out.println("자동 신고 기능이 지원되지 않습니다.");
+        }
+    }
+
+    void insertMemoryCard(int capacity) {
+        System.out.println("메모리 카드가 삽입되었습니다.");
+        System.out.println("용량은 " + capacity + "GB 입니다.");
+    }
+
+    int getVideoFileCount(int type) {
+        if (type == 1) { // 일반 영상
+            return 9;
+        } else if (type == 2) { // 이벤트 영상
+            return 1;
+        }
+        return 10;
+    }
 }
