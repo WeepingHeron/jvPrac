ac2ebaa581d2fdd3936516713309ab41b3aea2c4
WeepingHeron <kimtaehoon_98@naver.com>
2023-09-29 10:29:03 +0900

    _09_GetterSetter

--- src/chap_07/_09_GetterSetter.java
+++ src/chap_07/_09_GetterSetter.java
@@ -23,5 +23,7 @@ public class _09_GetterSetter {
 
         System.out.println("가격 : " + b2.getPrice() + "원");
         System.out.println("해상도 : " + b2.getResolution());
+
+        b2.price = -5000;
     }
 }
