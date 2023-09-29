b9df775dfff28743803471f963d442180f85bac4
WeepingHeron <kimtaehoon_98@naver.com>
2023-09-29 10:09:16 +0900

    BlackBox

--- src/chap_07/BlackBox.java
+++ src/chap_07/BlackBox.java
@@ -5,8 +5,27 @@ public class BlackBox {
     String resolution; // 해상도
     int price; //가격
     String color; //색상
+    int serialNumber; // 시리얼 번호
+
+    static int counter = 0; // 시리얼 번호를 생성해주는 역할 (처음엔 0이었다가 ++ 연산을 통해서 값을 증가)
     static boolean canAutoReport = false; // 자동 신고 기능
 
+    BlackBox() {
+        System.out.println("기본 생성자 호출");
+        this.serialNumber = ++counter;
+        System.out.println("새로운 시리얼 넘버를 발급받았습니다 : " + this.serialNumber);
+    }
+
+    BlackBox(String modelName, String resolution, int price, String color) {
+        this(); // 기본 생성자 호출
+
+        System.out.println("사용자 정의 생성자 호출");
+        this.modelName = modelName;
+        this.resolution = resolution;
+        this.price = price;
+        this.color = color;
+    }
+    
     void autoReport() {
         if (canAutoReport) {
             System.out.println("충돌이 감지되어 자동으로 신고합니다.");
