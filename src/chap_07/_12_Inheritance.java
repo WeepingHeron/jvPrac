2b67e7be8d97b47ba9158a02ecc5b2eafb16600a
WeepingHeron <kimtaehoon_98@naver.com>
2023-09-30 18:36:50 +0900

    _12_Inheritance

---
+++ src/chap_07/_12_Inheritance.java
@@ -0,0 +1,26 @@
+package chap_07;
+
+import chap_07.camera.Camera;
+import chap_07.camera.FactoryCam;
+import chap_07.camera.SpeedCam;
+
+public class _12_Inheritance {
+    public static void main(String[] args) {
+        // 상속
+        Camera camera = new Camera();
+        FactoryCam factoryCam = new FactoryCam();
+        SpeedCam speedCam = new SpeedCam();
+
+        System.out.println(camera.name);
+        System.out.println(factoryCam.name);
+        System.out.println(speedCam.name);
+
+        camera.takePicture();
+        factoryCam.recordVideo();
+        speedCam.takePicture();
+
+        factoryCam.detectFire();
+        speedCam.checkSpeed();
+        speedCam.recognizeLicensePlate();
+    }
+}
