7aebeadbf90d160e0457ec243bcdcad738410db7
WeepingHeron <kimtaehoon_98@naver.com>
2023-10-01 17:39:59 +0900

    _02_Interface

---
+++ src/chap_08/_02_Interface.java
@@ -0,0 +1,33 @@
+package chap_08;
+
+import chap_08.camera.FactoryCam;
+import chap_08.detector.*;
+import chap_08.reporter.*;
+
+public class _02_Interface {
+    public static void main(String[] args) {
+        // 인터페이스
+        Reportable normalReporter = new NormalReporter();
+        normalReporter.report();
+
+        Reportable videoReporter = new VideoReporter();
+        videoReporter.report();
+
+        System.out.println("-----------------------");
+
+        Detectable fireDetector = new FireDetector();
+        fireDetector.detect();
+
+        Detectable advancedFireDetector = new AdvancedFireDetector();
+        advancedFireDetector.detect();
+
+        System.out.println("-----------------------");
+
+        FactoryCam factoryCam = new FactoryCam();
+        factoryCam.setDetector(advancedFireDetector);
+        factoryCam.setReporter(videoReporter);
+
+        factoryCam.detect();
+        factoryCam.report();
+    }
+}
