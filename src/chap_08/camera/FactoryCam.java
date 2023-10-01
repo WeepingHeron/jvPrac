bcced0b6f4b7604fb211bf5c08961e2564636e25
WeepingHeron <kimtaehoon_98@naver.com>
2023-10-01 17:37:59 +0900

    FactoryCam

--- src/chap_08/camera/FactoryCam.java
+++ src/chap_08/camera/FactoryCam.java
@@ -1,8 +1,32 @@
 package chap_08.camera;
 
-public class FactoryCam extends Camera {
+import chap_08.detector.Detectable;
+import chap_08.reporter.Reportable;
+
+public class FactoryCam extends Camera implements Detectable, Reportable {
+    private Detectable detector;
+    private Reportable reporter;
+
+    public void setDetector(Detectable detector) {
+        this.detector = detector;
+    }
+
+    public void setReporter(Reportable reporter) {
+        this.reporter = reporter;
+    }
+
     @Override
     public void showMainFeature() {
         System.out.println("화재 감지");
+    }
+
+    @Override
+    public void detect() {
+        detector.detect();
+    }
+
+    @Override
+    public void report() {
+        reporter.report();
     }
 }
