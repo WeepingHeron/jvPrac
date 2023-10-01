f670fa171973310fefa4d931cb54f73e9fab5327
WeepingHeron <kimtaehoon_98@naver.com>
2023-10-01 17:13:02 +0900

    Camera

---
+++ src/chap_08/camera/Camera.java
@@ -0,0 +1,13 @@
+package chap_08.camera;
+
+public abstract class Camera {
+    public void takePicture() {
+        System.out.println("사진을 촬영합니다.");
+    }
+
+    public void recordVideo() {
+        System.out.println("동영상을 녹화합니다.");
+    }
+
+    public abstract void showMainFeature(); // 구현해야 하는 메소드
+}
