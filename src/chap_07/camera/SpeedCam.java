981ba94c3a25eddb03e3ee52aaf2f8402310ba6f
WeepingHeron <kimtaehoon_98@naver.com>
2023-10-01 15:34:20 +0900

    SpeedCam

--- src/chap_07/camera/SpeedCam.java
+++ src/chap_07/camera/SpeedCam.java
@@ -4,7 +4,14 @@ package chap_07.camera;
 public class SpeedCam extends Camera { // 자식 클래스
 
     public SpeedCam() {
-        this.name = "과속단속 카메라";
+        // this.name = "과속단속 카메라";
+        super("과속단속 카메라");
+    }
+
+    public void takePicture() {
+        super.takePicture();
+        checkSpeed();
+        recognizeLicensePlate();
     }
 
     public void checkSpeed() {
