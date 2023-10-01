6574869c87b75e5944fd86798bbdc627fa8fed49
WeepingHeron <kimtaehoon_98@naver.com>
2023-10-01 18:10:31 +0900

    SpeedCam

--- src/chap_08/camera/SpeedCam.java
+++ src/chap_08/camera/SpeedCam.java
@@ -1,16 +1,30 @@
 package chap_08.camera;
 
+import chap_08.detector.Detectable;
+import chap_08.reporter.Reportable;
+
 public class SpeedCam extends Camera {
+    private Detectable detector;
+    private Reportable reporter;
+
+    public void setDetector(Detectable detector) {
+        this.detector = detector;
+    }
+
+    public void setReporter(Reportable reporter) {
+        this.reporter = reporter;
+    }
+
     @Override
     public void showMainFeature() {
         System.out.println("속도 측정, 번호 인식");
     }
 
     public void detect() {
-        System.out.println("사고를 감지합니다.");
+        detector.detect();
     }
 
     public void report() {
-        System.out.println("사고 신고를 진행합니다.");
+        reporter.report();
     }
 }
