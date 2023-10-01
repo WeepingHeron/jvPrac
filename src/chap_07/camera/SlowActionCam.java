a57046b250684dc2addf7817dc7964f47945032a
WeepingHeron <kimtaehoon_98@naver.com>
2023-10-01 15:57:07 +0900

    SlowActionCam

---
+++ src/chap_07/camera/SlowActionCam.java
@@ -0,0 +1,22 @@
+package chap_07.camera;
+
+public class SlowActionCam {
+//    public SlowActionCam() {
+//        this.name = "슬로우 액션 카메라";
+//    }
+
+//    public void makeVideo() {
+//        System.out.println(this.name + " : "
+//                + this.lens + "로 촬영한 영상을 통해 멋진 슬로우모드 비디오를 제작합니다.");
+//    }
+    
+    public String name;
+    
+    public SlowActionCam() {
+        this.name = "슬로우 액션 카메라";
+    }
+    
+    public void makeVideo() {
+        System.out.println("자체 개발한 비디오 제작 기능");
+    }
+}
