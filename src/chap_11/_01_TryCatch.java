659ebb3ef05cfacceff10bc8d08444512d8b182d
WeepingHeron <kimtaehoon_98@naver.com>
2023-10-02 20:21:28 +0900

    _01_TryCatch

---
+++ src/chap_11/_01_TryCatch.java
@@ -0,0 +1,23 @@
+package chap_11;
+
+public class _01_TryCatch {
+    public static void main(String[] args) {
+        // 예외 처리
+        // 오류 : 컴파일 오류, 런타임 오류 (에러 error, 예외 exception)
+
+        try {
+            // System.out.println(3 / 0);
+
+//            int[] arr = new int[3];
+//            arr[5] = 100;
+
+            Object obj = "test";
+            System.out.println((int) obj);
+        } catch (Exception e) {
+            System.out.println("이런 문제가 발생했어요 => " + e.getMessage());
+            e.printStackTrace();
+        }
+
+        System.out.println("프로그램 정상 종료");
+    }
+}
