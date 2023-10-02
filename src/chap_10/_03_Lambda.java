d0eec3e74ce5d594f1c9c1fd0088cd1f5686f9e6
WeepingHeron <kimtaehoon_98@naver.com>
2023-10-02 16:44:29 +0900

    _03_Lambda

---
+++ src/chap_10/_03_Lambda.java
@@ -0,0 +1,26 @@
+package chap_10;
+
+public class _03_Lambda {
+    public static void main(String[] args) {
+        // 람다식
+        // (전달값1, 전달값2, ...) -> { 코드 }
+    }
+
+//    public void print() {
+//        String s = "test";
+//        System.out.println(s);
+//    }
+
+//    () -> {
+//        String s = "test";
+//        System.out.println(s);
+//    }
+
+//    public void print (String s) {
+//        System.out.println(s);
+//    }
+
+//    s -> System.out.println(s)
+
+//    (x, y) -> x + y
+}
