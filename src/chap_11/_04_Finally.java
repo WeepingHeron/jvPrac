bd3b30b33c10ab9d67642753b47a75c2b0e33811
WeepingHeron <kimtaehoon_98@naver.com>
2023-10-02 20:43:10 +0900

    _04_Finally

---
+++ src/chap_11/_04_Finally.java
@@ -0,0 +1,27 @@
+package chap_11;
+
+public class _04_Finally {
+    public static void main(String[] args) {
+        try {
+            System.out.println("택시의 문을 연다.");
+            throw new Exception("휴무 택시");
+            // System.out.println("택시에 탑승한다.");
+        } catch (Exception e) {
+            System.out.println("!! 문제 발생 : " + e.getMessage());
+        } finally {
+            System.out.println("택시의 문을 닫는다.");
+        }
+
+        // try + catch(s)
+        // try + catch(s) + finally
+        // try + finally
+
+        System.out.println("------------------");
+
+        try {
+            System.out.println(3 / 0);
+        } finally {
+            System.out.println("프로그램 정상 종료");
+        }
+    }
+}
