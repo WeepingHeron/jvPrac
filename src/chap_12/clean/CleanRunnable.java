5e7eb3a1e59e70b43ffd24ca271392c35b2a4c8e
WeepingHeron <kimtaehoon_98@naver.com>
2023-10-03 12:47:40 +0900

    CleanRunnable

--- src/chap_12/clean/CleanRunnable.java
+++ src/chap_12/clean/CleanRunnable.java
@@ -3,9 +3,9 @@ package chap_12.clean;
 public class CleanRunnable implements Runnable {
     @Override
     public void run() {
-        System.out.println("-- 직원 청소 시작 (Thread) --");
+        System.out.println("-- 직원 청소 시작 (Runnable) --");
         for (int i = 2; i <= 10; i += 2) {
-            System.out.println("(직원) " + i + "번방 청소 중 (Thread)");
+            System.out.println("(직원) " + i + "번방 청소 중 (Runnable)");
 
             try {
                 Thread.sleep(1000);
@@ -13,6 +13,6 @@ public class CleanRunnable implements Runnable {
                 throw new RuntimeException(e);
             }
         }
-        System.out.println("-- 직원 청소 끝 (Thread) --");
+        System.out.println("-- 직원 청소 끝 (Runnable) --");
     }
 }
